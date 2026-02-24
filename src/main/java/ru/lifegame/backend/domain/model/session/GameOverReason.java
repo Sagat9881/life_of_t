28f6f6aa8c3d4b9a6e0b38ddddb4c218b71bc1f7
Александр <50985582+Sagat9881@users.noreply.github.com>
2026-02-24 21:38:22 +0300

    refactor: move GameOverReason to session aggregate
    - Enum representing game over reasons
    - Part of session aggregate

---
+++ src/main/java/ru/lifegame/backend/domain/model/session/GameOverReason.java
@@ -0,0 +1,12 @@
+package ru.lifegame.backend.domain.model.session;
+
+/**
+ * Enumeration of possible game over reasons.
+ */
+public enum GameOverReason {
+    BURNOUT,
+    DIVORCE,
+    ISOLATION,
+    PET_DEATH,
+    BANKRUPTCY
+}
