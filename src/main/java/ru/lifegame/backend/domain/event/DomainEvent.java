137079f2b1c35ac6dc428e42cb541e9f7a609431
Александр <50985582+Sagat9881@users.noreply.github.com>
2026-02-27 11:16:55 +0300

    Delete old DomainEvent interface - replaced by domain.event.domain package

--- src/main/java/ru/lifegame/backend/domain/event/DomainEvent.java
+++
@@ -1,7 +0,0 @@
-package ru.lifegame.backend.domain.event;
-
-public sealed interface DomainEvent permits
-        ActionExecutedEvent, ConflictTriggeredEvent, ConflictTacticAppliedEvent,
-        ConflictResolvedEvent, DayEndedEvent, GameOverEvent,
-        RelationshipBrokenEvent, QuestProgressUpdatedEvent,
-        EventTriggeredEvent, EndingAchievedEvent {}
