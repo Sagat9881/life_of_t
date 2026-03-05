86c162e2b263770f7c2a8fab49bb210323455d01
Александр <50985582+Sagat9881@users.noreply.github.com>
2026-03-05 15:03:10 +0300

    fix: add missing bean definitions from old config (scanner, parser, use-cases)

--- asset-generator/src/main/java/ru/lifegame/assets/config/AssetGeneratorConfig.java
+++ asset-generator/src/main/java/ru/lifegame/assets/config/AssetGeneratorConfig.java
@@ -3,9 +3,13 @@ package ru.lifegame.assets.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.lifegame.assets.application.usecase.GenerateLayeredAssetUseCase;
+import ru.lifegame.assets.application.usecase.ScanMissingSpecsUseCase;
+import ru.lifegame.assets.application.usecase.UnifyXmlSpecsUseCase;
 import ru.lifegame.assets.domain.service.AssetGenerationService;
 import ru.lifegame.assets.infrastructure.generator.LayeredAssetGenerator;
 import ru.lifegame.assets.infrastructure.generator.UniversalPixelRenderer;
+import ru.lifegame.assets.infrastructure.parser.XmlAssetSpecParser;
+import ru.lifegame.assets.infrastructure.scanner.PromptDirectoryScanner;
 import ru.lifegame.assets.infrastructure.writer.AtlasConfigWriter;
 import ru.lifegame.assets.infrastructure.writer.PngLayerWriter;
 import ru.lifegame.assets.infrastructure.writer.WebpAtlasWriter;
@@ -34,6 +38,16 @@ public class AssetGeneratorConfig {
     }
 
     @Bean
+    public XmlAssetSpecParser xmlAssetSpecParser() {
+        return new XmlAssetSpecParser();
+    }
+
+    @Bean
+    public PromptDirectoryScanner promptDirectoryScanner() {
+        return new PromptDirectoryScanner();
+    }
+
+    @Bean
     public AssetGenerationService assetGenerationService(
             UniversalPixelRenderer renderer,
             PngLayerWriter pngWriter,
@@ -46,5 +60,17 @@ public class AssetGeneratorConfig {
     public GenerateLayeredAssetUseCase generateLayeredAssetUseCase(
             AssetGenerationService service) {
         return new GenerateLayeredAssetUseCase(service);
+    }
+
+    @Bean
+    public ScanMissingSpecsUseCase scanMissingSpecsUseCase(
+            PromptDirectoryScanner scanner) {
+        return new ScanMissingSpecsUseCase(scanner);
+    }
+
+    @Bean
+    public UnifyXmlSpecsUseCase unifyXmlSpecsUseCase(
+            XmlAssetSpecParser parser) {
+        return new UnifyXmlSpecsUseCase(parser);
     }
 }
